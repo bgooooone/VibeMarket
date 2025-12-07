7c74c7b788ef27bbf10d56f37d3a2c983440e91c
bgooooone <1937114321@qq.com>
2025-12-07 21:37:31 +0800

    优化分页和图片上传无法显示的问题

--- VibeMarket/backend/src/main/java/com/shopping/product/mapper/ProductMapper.java
+++ VibeMarket/backend/src/main/java/com/shopping/product/mapper/ProductMapper.java
@@ -3,6 +3,7 @@ package com.shopping.product.mapper;
 import com.shopping.product.entity.Product;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 import java.util.Map;
@@ -20,5 +21,16 @@ public interface ProductMapper {
     int deleteById(Long id);
     int countAll();
     List<Map<String, Object>> countByCategory();
+    @Select("SELECT * FROM product WHERE " +
+            "name LIKE CONCAT('%', #{keyword}, '%') OR " +
+            "description LIKE CONCAT('%', #{keyword}, '%') " +
+            "ORDER BY sales DESC LIMIT 10")
+    List<Product> selectByKeywords(@Param("keywords") List<String> keywords);
+
+    @Select("SELECT * FROM product WHERE " +
+            "name LIKE CONCAT('%', #{query}, '%') OR " +
+            "description LIKE CONCAT('%', #{query}, '%') " +
+            "ORDER BY create_time DESC LIMIT 5")
+    List<Product> selectBySearch(@Param("query") String query);
 }
 
